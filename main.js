86c366cee60b7f2d495da78c376f373c4ffc7304
mari2gfrg <138078457+mari2gfrg@users.noreply.github.com>
2023-11-23 20:22:56 -0300

    Update main.js
    aa

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio)  {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla')
+const listadeTeclas = document.querySelectorAll('tecla');
+
+//para
+for (let contador = 0; contador < listadeTeclas.length; contador++) {
+
+    const tecla = listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio ='#som_${instrumento}'; //template string
+
+    tecla.onclick = function ()   {
+        tocaSom(idAudio);
+    }
+
+tecla.onkeydown = function () {
+    tecla.classList.add('ativa')
+} 
+
+
+
+
